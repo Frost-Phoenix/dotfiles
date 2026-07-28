74e0197542e7d1bd79ee82a197f7df7d3add7815
Frost-Phoenix <67cyril6767@gmail.com>
2026-07-28 10:33:00 +0200

    config(hyprland): add app auto start

--- hypr/.config/hypr/modules/auto-start.lua
+++ hypr/.config/hypr/modules/auto-start.lua
@@ -4,4 +4,6 @@
 
 hl.on("hyprland.start", function () 
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("uwsm-app -- flatpak run app.zen_browser.zen")
+  hl.exec_cmd("uwsm-app -- flatpak run io.gitlab.news_flash.NewsFlash")
 end)
